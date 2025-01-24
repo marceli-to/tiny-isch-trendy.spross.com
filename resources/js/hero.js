21ed3affa4c808d26caec2bb8c311455cde41576
Marcel Stadelmann <m@marceli.to>
2025-01-24 20:26:41 +0100

    wip

--- resources/js/hero.js
+++ resources/js/hero.js
@@ -6,53 +6,59 @@ document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger);
 
   // Create main timeline for hero animation
-  // const tl = gsap.timeline({
-  //   scrollTrigger: {
-  //     trigger: "[data-hero-section]",
-  //     start: "top top",
-  //     end: "+=50%",
-  //     scrub: true,
-  //     pin: true,
-  //     pinSpacing: true,
-  //     markers: true
-  //   }
-  // });
-
-  // // Headline animation
-  // tl.to("[data-hero-headline]", {
-  //   scale: 0.4,
-  //   top: "20px",
-  //   duration: 1,
-  //   ease: "none"
-  // })
-
-  // // Image animation
-  // .to("[data-hero-image]", {
-  //   y: "-50vh",
-  //   duration: 1,
-  //   ease: "none"
-  // }, "<")
-
-  // // Badge animation
-  // .to("[data-hero-badge]", {
-  //   y: "-20vh",
-  //   duration: 1,
-  //   ease: "none"
-  // }, "<");
-
-  gsap.registerPlugin(ScrollTrigger);
- 
-  gsap.to('.hero-headline', {
+  const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: '.hero-headline',
-      start: 'center center',
-      end: '+=700',
+      trigger: "[data-hero-section]",
+      start: "top top",
+      end: "+=50%",
       scrub: true,
-    },
-    scale: 0.3,
-    duration: 1,
-    transformOrigin: 'left bottom' // Changed from 'left top' to 'left bottom'
+      pin: true,
+      //pinSpacing: true,
+      // markers: true
+    }
   });
+
+  tl.to("[data-hero-wrapper]", {
+    y: "-30vh",
+    duration: 1,
+    ease: "none"
+  })
+
+  // Headline animation
+  tl.to("[data-hero-headline]", {
+    scale: 0.2,
+    y: "-10vh",
+    duration: .75,
+    ease: "none"
+  }, "<")
+
+  // Image animation
+  .to("[data-hero-image]", {
+    y: "-40vh",
+    duration: 1,
+    ease: "none"
+  }, "<")
+
+  // Badge animation
+  .to("[data-hero-badge]", {
+    y: "-20vh",
+    duration: 1,
+    ease: "none"
+  }, "<");
+
+  // gsap.registerPlugin(ScrollTrigger);
+ 
+  // gsap.to('.hero-headline', {
+  //   scrollTrigger: {
+  //     trigger: '.hero-headline',
+  //     start: 'center center',
+  //     end: '+=700',
+  //     scrub: true,
+  //   },
+  //   scale: 0.3,
+  //   duration: 1,
+  //   transformOrigin: 'left bottom' // Changed from 'left top' to 'left bottom'
+  // });
 
   
 
